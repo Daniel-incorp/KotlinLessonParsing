9f0b748e3742926c217886062467eb5eca71be02
Daniel <daniel.work9@mail.ru>
2025-07-19 13:02:01 +0300

    KS-5-8-task8 work with quotes parsing (don't work)

--- src/main/kotlin/lesson_5/lesson5_task8.kt
+++ src/main/kotlin/lesson_5/lesson5_task8.kt
@@ -2,17 +2,13 @@ package lesson_5
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 fun main() {
     val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
 
-    val quotes: Elements = doc.select("div.sc-2aegk7-2.bzpNIu")
+    val quotes: Elements = doc.select("article.sc-14uz67c-0.ccmjFA")
 
-    for (quote: Element in quotes) {
-        println("${quote.attr("title")}\n${quote.absUrl("href")}")
-    }
-
+    quotes.forEach { println("${it.text("article")}") }
 }
 
